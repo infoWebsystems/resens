18aa6ae539295d53a7a5ec8eb95d38fb7cde6593
Victor Kovalchuk <vict02r05@gmail.com>
2024-08-21 16:22:15 +0300

    bug fixing; change language detecting

--- assets/custom-header-lang.js
+++ assets/custom-header-lang.js
@@ -1,13 +1,8 @@
 function getCurrentLanguage() {
-  let url = window.location.href;
-
-  let match = url.match(/\/([a-z]{2})(?:\/|$)/);
-
-  return match ? match[1] : null;
+  return document.documentElement.getAttribute('lang')
 }
 
 let currentLanguage = getCurrentLanguage() ? getCurrentLanguage() : 'uk';
-
 
 if (currentLanguage === 'uk') {
   document.querySelector('.language-switcher-header .disclosure').insertAdjacentHTML('afterbegin', '<a href="#" aria-current="true" hreflang="en" lang="en" data-value="en">English <span class="divider">|</span> EN</a>');
