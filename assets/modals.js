c48bef8b3faaf5a6bca2a65c1688484eec67cfe6
Victor Kovalchuk <vict02r05@gmail.com>
2024-10-13 12:02:50 +0300

    add thank you you modal on success submission

--- assets/modals.js
+++ assets/modals.js
@@ -1,97 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const modal = document.getElementById('sendResumeForm');
-  const overlay = document.querySelector('.form-overlay');
-  const btnCloseModal = modal.querySelector('.form-modal-close');
-  const btnsOpenModal = document.querySelectorAll('#onSendResumeFormOpen');
+  function formModalInit (modalElem, closeElem, triggerElem) {
+    const modal = document.getElementById(modalElem);
+    const overlay = document.querySelector('.form-overlay');
+    const btnsCloseModal = modal.querySelectorAll(closeElem);
+    const btnsOpenModal = document.querySelectorAll(triggerElem);
 
-  const openModal = function () {
-    overlay.classList.remove('form-modal-hidden');
-    modal.classList.remove('form-modal-hidden');
-    document.querySelector('body').style.overflow = 'hidden';
-  }
-
-  const closeModal = function () {
-    overlay.classList.add('form-modal-hidden');
-    modal.classList.add('form-modal-hidden');
-    document.querySelector('body').style.overflow = 'auto';
-  }
-
-  for (let i = 0; i < btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', openModal);
-  }
-
-  btnCloseModal.addEventListener('click', closeModal);
-  overlay.addEventListener('click', closeModal);
-
-  document.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape' && !modal.classList.contains('form-modal-hidden')) {
-      closeModal();
+    const openModal = function () {
+      overlay.classList.remove('hidden');
+      modal.classList.remove('hidden');
+      document.querySelector('body').style.overflow = 'hidden';
     }
-  })
-})
 
-document.addEventListener("DOMContentLoaded", () => {
-  const modal = document.getElementById('partnershipForm');
-  const overlay = document.querySelector('.form-overlay');
-  const btnCloseModal = modal.querySelector('.form-modal-close');
-  const btnsOpenModal = document.querySelectorAll('#onPartnershipForm');
-
-  const openModal = function () {
-    overlay.classList.remove('form-modal-hidden');
-    modal.classList.remove('form-modal-hidden');
-    document.querySelector('body').style.overflow = 'hidden';
-  }
-
-  const closeModal = function () {
-    overlay.classList.add('form-modal-hidden');
-    modal.classList.add('form-modal-hidden');
-    document.querySelector('body').style.overflow = 'auto';
-  }
-
-  for (let i = 0; i < btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', openModal);
-  }
-
-  btnCloseModal.addEventListener('click', closeModal);
-  overlay.addEventListener('click', closeModal);
-
-  document.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape' && !modal.classList.contains('form-modal-hidden')) {
-      closeModal();
+    const closeModal = function () {
+      overlay.classList.add('hidden');
+      modal.classList.add('hidden');
+      document.querySelector('body').style.overflow = 'auto';
     }
-  })
-})
 
-document.addEventListener("DOMContentLoaded", () => {
-  const modal = document.getElementById('thankYouModal');
-  const overlay = document.querySelector('.form-overlay');
-  const btnCloseModal = modal.querySelector('.modal-thank-you-close');
-  const btnContinueModal = modal.querySelector('.modal-thank-you-button');
-  const btnsOpenModal = document.querySelectorAll('#onThankYouModal');
-
-  const openModal = function () {
-    overlay.classList.remove('form-modal-hidden');
-    modal.classList.remove('modal-thank-you-hidden');
-    document.querySelector('body').style.overflow = 'hidden';
-  }
-
-  const closeModal = function () {
-    overlay.classList.add('form-modal-hidden');
-    modal.classList.add('modal-thank-you-hidden');
-    document.querySelector('body').style.overflow = 'auto';
-  }
-
-  for (let i = 0; i < btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', openModal);
-  }
-
-  btnCloseModal.addEventListener('click', closeModal);
-  btnContinueModal.addEventListener('click', closeModal);
-  overlay.addEventListener('click', closeModal);
-
-  document.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape' && !modal.classList.contains('modal-thank-you-hidden')) {
-      closeModal();
+    for (let i = 0; i < btnsOpenModal.length; i++) {
+      btnsOpenModal[i].addEventListener('click', openModal);
     }
-  })
+
+    btnsCloseModal.forEach(closeBtn => {
+      closeBtn.addEventListener('click', closeModal);
+    })
+
+    overlay.addEventListener('click', closeModal);
+
+    document.addEventListener('keydown', function (e) {
+      if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+      }
+    })
+  }
+
+  formModalInit('sendResumeForm', '.form-modal-close', '.onSendResumeFormOpen');
+  formModalInit('partnershipForm', '.form-modal-close', '.onPartnershipForm');
+  formModalInit('thankYouModal', '.modal-thank-you-close', '.onThankYouModal');
+
+  function submitForm (formId) {
+    document.querySelector(`${formId} form`).addEventListener('submit', function (e) {
+      console.log(formId)
+      e.preventDefault();
+    
+      var formData = new FormData(this);
+    
+      fetch(this.action, {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => {
+        if (response.ok) {
+          console.log('Success: Form submitted successfully!');
+          this.closest(formId).classList.add('hidden');
+          document.getElementById('thankYouModal').classList.remove('hidden');
+        } else {
+          console.log('Error: Form submission failed.');
+        }
+      })
+      .catch(error => console.log('Error:', error));
+    });
+  }
+
+  submitForm('#sendResumeForm');
+  submitForm('#partnershipForm');
 })
